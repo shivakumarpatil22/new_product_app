ab1bbf3ae48d02d539771310407aa1d52d445b77
Shiva Kumar <shivakumar@Shivas-MacBook-Air.local>
2017-05-25 16:03:28 +0530

    Change email setup credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'shivakumarpatilqa@gmail.com',
-    password:             'rubyonrails',
+    user_name:            '<username>',
+    password:             '<password>',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
